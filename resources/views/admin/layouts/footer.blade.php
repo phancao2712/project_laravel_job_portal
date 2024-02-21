1cd6a3f77e69994acce9eeda4eaa04adbb7a1ff8
phancao2712 <phancaoa1@gmail.com>
2024-02-21 09:25:12 +0700

    'template_admin'

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By PhanKao
+    </div>
+    <div class="footer-right">
+    </div>
+  </footer>
