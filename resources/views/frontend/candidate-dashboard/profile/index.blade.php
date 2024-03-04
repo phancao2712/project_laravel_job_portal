102edc41940cc7308c38b940a1e6331e7be739da
phancao2712 <phancaoa1@gmail.com>
2024-03-04 09:20:19 +0700

    setup view and create table candidateEducation

--- resources/views/frontend/candidate-dashboard/profile/index.blade.php
+++ resources/views/frontend/candidate-dashboard/profile/index.blade.php
@@ -115,9 +115,68 @@
             </div>
         </div>
     </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="educationModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Education</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form id="educationForm">
+                        @csrf
+                        <div class="row form-controler">
+                            <div class="col-md-12">
+                                <label for="company">Company *</label>
+                                <input type="text" name="company" class="form-control" required="">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="company">Department *</label>
+                                <input type="text" name="department" class="form-control" required="">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="designation">Designation *</label>
+                                <input type="text" name="designation" class="form-control" required="">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="company">Date start *</label>
+                                <input type="text" class="datepicker" name="start" class="form-control"
+                                    required="">
+                            </div>
+                            <div class="col-md-6">
+                                <label for="company">Date end *</label>
+                                <input type="text" class="datepicker" name="end" class="form-control"
+                                    required="">
+                            </div>
+                            <div class="col-md-12 mt-10 mb-10">
+                                <input id="remember_me" type="checkbox" class="form-check-input" name="current_working">
+                                <label class="form-check-label" for="remember">I am currently working</label>
+                            </div>
+                            <div class="col-md-12">
+                                <label for="company">Responsibility</label>
+                                <textarea name="responsibilites" id="" class="form-control"></textarea>
+                            </div>
+
+                            <div class="text-right">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Update Education</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @push('script')
     <script>
+        // reload data table
         function fetchExperience() {
             $.ajax({
                 method: 'GET',
--- resources/views/frontend/candidate-dashboard/profile/sections/experience.blade.php
+++ resources/views/frontend/candidate-dashboard/profile/sections/experience.blade.php
@@ -1,36 +1,84 @@
 <div class="tab-pane fade" id="pills-experience" role="tabpanel" aria-labelledby="pills-experience-tab" tabindex="0">
-    <div class="d-flex justify-content-between">
-        <h3>Experience</h3>
-        <div class="btn btn-primary" data-bs-toggle="modal" onclick="$('#experienceForm').trigger('reset'); eiditId=''; eidtMode = false;" data-bs-target="#staticBackdrop">Add experience</div>
-    </div>
-    <br>
-    <table class="table table-striped" id="experienceTable">
-        <thead>
-            <tr>
-                <th scope="col">Company</th>
-                <th scope="col">Department</th>
-                <th scope="col">Designation</th>
-                <th scope="col">Period</th>
-                <th scope="col" style="width:15%;">Actions</th>
-            </tr>
-        </thead>
-        <tbody class="experience-tbody">
-            @foreach ($experienceCandidate as $exp)
+    <div class="">
+        <div class="d-flex justify-content-between">
+            <h3>Experience</h3>
+            <div class="btn btn-primary" data-bs-toggle="modal" onclick="$('#experienceForm').trigger('reset'); eiditId=''; eidtMode = false;" data-bs-target="#staticBackdrop">Add experience</div>
+        </div>
+        <br>
+        <table class="table table-striped" id="experienceTable">
+            <thead>
                 <tr>
-                    <td>{{ $exp->company }}</td>
-                    <td>{{ $exp->department }}</td>
-                    <td>{{ $exp->designation }}</td>
-                    <td>{{ date('d-m-Y', strtotime($exp->start)) }} -
-                        {{ $exp->current_working === 1 ? 'Current' : date('d-m-Y', strtotime($exp->end)) }}</td>
-                    <td>
-                        <a href="{{ route('candidate.experience.edit', $exp->id) }}" data-bs-toggle="modal"
-                            data-bs-target="#staticBackdrop" class="btn editExperience btn-sm btn-primary"><i
-                                class="fa-solid fa-pen-to-square"></i></a>
-                        <a href="{{ route('candidate.experience.destroy', $exp->id) }}"
-                            class="btn btn-sm btn-danger delete-experience"><i class="fa-solid fa-trash"></i></a>
-                    </td>
+                    <th scope="col">Company</th>
+                    <th scope="col">Department</th>
+                    <th scope="col">Designation</th>
+                    <th scope="col">Period</th>
+                    <th scope="col" style="width:15%;">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody class="experience-tbody">
+                @forelse ($experienceCandidate as $exp)
+                    <tr>
+                        <td>{{ $exp->company }}</td>
+                        <td>{{ $exp->department }}</td>
+                        <td>{{ $exp->designation }}</td>
+                        <td>{{ date('d-m-Y', strtotime($exp->start)) }} -
+                            {{ $exp->current_working === 1 ? 'Current' : date('d-m-Y', strtotime($exp->end)) }}</td>
+                        <td>
+                            <a href="{{ route('candidate.experience.edit', $exp->id) }}" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop" class="btn editExperience btn-sm btn-primary"><i
+                                    class="fa-solid fa-pen-to-square"></i></a>
+                            <a href="{{ route('candidate.experience.destroy', $exp->id) }}"
+                                class="btn btn-sm btn-danger delete-experience"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">Not Found Data!</td>
+                        </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+
+    {{-- Education --}}
+    <div class="mt-60">
+        <div class="d-flex justify-content-between">
+            <h3>Education</h3>
+            <div class="btn btn-primary" data-bs-toggle="modal" onclick="$('#educationForm').trigger('reset'); eiditId=''; eidtMode = false;" data-bs-target="#educationModal">Add education</div>
+        </div>
+        <br>
+        <table class="table table-striped" id="educationTable">
+            <thead>
+                <tr>
+                    <th scope="col">Company</th>
+                    <th scope="col">Department</th>
+                    <th scope="col">Designation</th>
+                    <th scope="col">Period</th>
+                    <th scope="col" style="width:15%;">Actions</th>
+                </tr>
+            </thead>
+            <tbody class="education-tbody">
+                @forelse ($experienceCandidate as $exp)
+                    <tr>
+                        <td>{{ $exp->company }}</td>
+                        <td>{{ $exp->department }}</td>
+                        <td>{{ $exp->designation }}</td>
+                        <td>{{ date('d-m-Y', strtotime($exp->start)) }} -
+                            {{ $exp->current_working === 1 ? 'Current' : date('d-m-Y', strtotime($exp->end)) }}</td>
+                        <td>
+                            <a href="" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop" class="btn editeducation btn-sm btn-primary"><i
+                                    class="fa-solid fa-pen-to-square"></i></a>
+                            <a href=""
+                                class="btn btn-sm btn-danger delete-education"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                    </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center">Not Found Data!</td>
+                        </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </div>
